691aff27ee7f7df9b4f03b71ae14bcf57dd1273e
Reino Muhl <reino.muhl@gmail.com>
2023-04-25 20:47:34 -0400

    Remove unused libs

--- packages/hooks/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface CouponDetails {
-  promotionCode: string;
-  type: string;
-  durationInMonths: number | null;
-  valid: boolean;
-  discountAmount?: number;
-  expired: boolean;
-  maximallyRedeemed: boolean;
-}
